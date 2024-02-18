368a57da0bf29fd339ba599db5e51d3f32ca4008
Antonino Scordino <135661010+AntoninoScordino@users.noreply.github.com>
2024-02-18 23:24:28 +0100

    Pacman: Add architecture flags
    Change-Id: Iedb47238b66e4527a4be82de49c2398cbdb95b55

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,5 +9,12 @@ DEVICE_PATH := device/nothing/Pacman
 # Asserts
 TARGET_OTA_ASSERT_DEVICE := Pacman,PacmanEEA,PacmanIND,PacmanJPN
 
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-2a-dotprod
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_VARIANT := cortex-a76
+TARGET_CPU_VARIANT_RUNTIME := cortex-a76
+
 # Inherit the proprietary files
 include vendor/nothing/Pacman/BoardConfigVendor.mk
