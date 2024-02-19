8c3a8fda36f2e14d02196f704d46715476593bc5
Giovanni Ricca <giovanniricca@proton.me>
2024-02-19 14:45:53 +0100

    Pacman: Inherit common MediaTek sepolicy
    Change-Id: Iacc23524057fce83684509da35bde27d3b4058f6

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -139,6 +139,9 @@ TARGET_USERIMAGES_USE_F2FS := true
 # Secuirty patch
 VENDOR_SECURITY_PATCH := 2023-10-05
 
+# SEPolicy
+include device/mediatek/sepolicy_vndr/SEPolicy.mk
+
 # Verified Boot
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
